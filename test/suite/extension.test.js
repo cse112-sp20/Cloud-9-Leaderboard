a965373e6ede40af9900d230c95c63aecef955ea
Daniel Tan <danielytan1@gmail.com>
2020-05-07 10:57:17 -0700

    Updating extension test to reflect QA pipeline
    Please review when possible

--- test/suite/extension.test.js
+++ test/suite/extension.test.js
@@ -5,11 +5,50 @@ const assert = require('assert');
 const vscode = require('vscode');
 // const myExtension = require('../extension');
 
+const Mocha = require('mocha');
+
+var globalNumber = 1
+
 suite('Extension Test Suite', () => {
+  console.log('s');
   vscode.window.showInformationMessage('Start all tests.');
 
-  test('Sample test', () => {
-    assert.equal(-1, [1, 2, 3].indexOf(5));
-    assert.equal(-1, [1, 2, 3].indexOf(0));
+  Mocha.describe('hooks', function() {
+    Mocha.before(function() {
+      // runs once before the first test in this block
+      console.log('HI')
+    });
+  
+    Mocha.after(function() {
+      // runs once after the last test in this block
+      console.log('BYE')
+    });
+  
+    Mocha.beforeEach(function() {
+      // runs before each test in this block
+      console.log('TEST ', globalNumber)
+    });
+  
+    Mocha.afterEach(function() {
+      // runs after each test in this block
+      console.log('END TEST', globalNumber)
+      globalNumber++;
+    });
+  
+    // test cases
+    test('hi', () => {
+      assert.equal(-1, [1, 2, 3].indexOf(5));
+      assert.equal(-1, [1, 2, 3].indexOf(0));
+    });
+  
+    test('hello', () => {
+      assert.equal(-1, [1, 2, 3].indexOf(5));
+      assert.equal(-1, [1, 2, 3].indexOf(0));
+    });
+  
+    test('howdy', () => {
+      assert.equal(-1, [1, 2, 3].indexOf(5));
+      assert.equal(-1, [1, 2, 3].indexOf(0));
+    });
   });
 });
