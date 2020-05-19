0f8ec6c331ed0aa727ccf04c1a82481dd22cd221
Ethan Yuan <etyuan@ucsd.edu>
2020-05-19 12:47:00 -0700

    Added check leaderboard.txt

--- src/util/Leaderboard.ts
+++ src/util/Leaderboard.ts
@@ -55,6 +55,27 @@ export async function displayLeaderboard() {
   await retrieveAllUserStats(writeToFile);
 
   let filePath = getLeaderboardFile();
+
+  try {
+    if (!fs.existsSync(filePath)) {
+      console.log('File not exist');
+      fs.writeFileSync(filePath, '', (err) => {
+        // throws an error, you could also catch it here
+        if (err) {
+          console.log('Error writing intially');
+          throw err;
+        }
+
+        // success case, the file was saved
+        console.log('Lyric saved!');
+      });
+    } else {
+      console.log('File exist');
+    }
+  } catch (err) {
+    console.error(err);
+  }
+
   workspace.openTextDocument(filePath).then((doc) => {
     // only focus if it's not already open
     window.showTextDocument(doc, ViewColumn.One, false).then((e) => {
