a01208eb3e37889d2cf39f3feb722968ff7db1c2
Ethan Yuan <etyuan@ucsd.edu>
2020-05-19 20:06:55 -0700

    test

--- out/lib/managers/WallClockManager.js
+++ out/lib/managers/WallClockManager.js
@@ -3,7 +3,6 @@ Object.defineProperty(exports, "__esModule", { value: true });
 exports.WallClockManager = void 0;
 const Util_1 = require("../Util");
 const vscode_1 = require("vscode");
-const SessionSummaryData_1 = require("../storage/SessionSummaryData");
 const TimeSummaryData_1 = require("../storage/TimeSummaryData");
 const KpmManager_1 = require("./KpmManager");
 const SECONDS_INTERVAL = 30;
@@ -21,26 +20,18 @@ class WallClockManager {
     }
     initTimer() {
         const kpmMgr = KpmManager_1.KpmManager.getInstance();
-        this._wctime = Util_1.getItem("wctime") || 0;
+        this._wctime = Util_1.getItem('wctime') || 0;
         setInterval(() => {
             // If the window is focused
             if (vscode_1.window.state.focused || kpmMgr.hasKeystrokeData()) {
                 // set the wctime (deprecated, remove one day when all plugins use time data info)
-                this._wctime = Util_1.getItem("wctime") || 0;
+                this._wctime = Util_1.getItem('wctime') || 0;
                 this._wctime += SECONDS_INTERVAL;
-                Util_1.setItem("wctime", this._wctime);
+                Util_1.setItem('wctime', this._wctime);
                 // update the file info file
                 TimeSummaryData_1.incrementEditorSeconds(SECONDS_INTERVAL);
             }
-            // dispatch to the various views (statusbar and treeview)
-            this.dispatchStatusViewUpdate();
         }, CLOCK_INTERVAL);
-    }
-    dispatchStatusViewUpdate() {
-        // update the status bar
-        SessionSummaryData_1.updateStatusBarWithSummaryData();
-        // update the code time metrics tree views
-        vscode_1.commands.executeCommand("codetime.refreshKpmTree");
     }
     getHumanizedWcTime() {
         return Util_1.humanizeMinutes(this._wctime / 60);
--- out/test/extension.test.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const chai_1 = require("chai");
-describe('Simple Test', () => {
-    it('can be initialized without an initializer', () => {
-        chai_1.expect(1).to.equal(1);
-    });
-});
-//# sourceMappingURL=extension.test.js.map
