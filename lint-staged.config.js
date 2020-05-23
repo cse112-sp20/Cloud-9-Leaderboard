486a6946f73239bf611dc16af6b03e049d9b8d70
Ethan Yuan <etyuan@ucsd.edu>
2020-05-23 15:04:24 -0700

    test precommit

--- lint-staged.config.js
+++ lint-staged.config.js
@@ -1,4 +1,7 @@
 // Configuration for lint-staged
 module.exports = {
-  '**/*.+(js|md|ts|css|sass|less|graphql|scss|json|vue)': ['prettier --write'],
+  '*.+(js|md|ts|css|sass|less|graphql|scss|json|vue)': [
+    'prettier --write',
+    'npm test',
+  ],
 };
