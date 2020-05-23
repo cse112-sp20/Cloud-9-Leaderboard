bae91272d724210a0773a8037f1f9ea4bd9f2234
Ethan Yuan <etyuan@ucsd.edu>
2020-05-23 00:08:42 -0700

    Rename

--- lint-staged.config.js
+++ lint-staged.config.js
@@ -1,7 +1,8 @@
 // Configuration for lint-staged
 module.exports = {
-  '**/*.+(js|md|ts|css|sass|less|graphql|yml|yaml|scss|json|vue)': [
+  '**/*.+(js|md|ts|css|sass|less|graphql|scss|json|vue)': [
     'eslint --fix',
     'prettier --write',
+    'npm test',
   ],
 };
