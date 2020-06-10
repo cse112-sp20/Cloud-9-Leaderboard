992b10086977e514ffc01618cbfe5bfc6091f5e7
Ethan Yuan <etyuan@ucsd.edu>
2020-06-09 18:58:45 -0700

    Change tsconfig

--- out/test-resources/VSCode-win32-x64-archive/resources/app/extensions/search-result/src/extension.js
+++ out/test-resources/VSCode-win32-x64-archive/resources/app/extensions/search-result/src/extension.js
@@ -18,98 +18,128 @@ const vscode = require("vscode");
 const pathUtils = require("path");
 const FILE_LINE_REGEX = /^(\S.*):$/;
 const RESULT_LINE_REGEX = /^(\s+)(\d+)(:| )(\s+)(.*)$/;
-const SEARCH_RESULT_SELECTOR = { language: 'search-result' };
-const DIRECTIVES = ['# Query:', '# Flags:', '# Including:', '# Excluding:', '# ContextLines:'];
-const FLAGS = ['RegExp', 'CaseSensitive', 'IgnoreExcludeSettings', 'WordMatch'];
+const SEARCH_RESULT_SELECTOR = { language: "search-result" };
+const DIRECTIVES = [
+    "# Query:",
+    "# Flags:",
+    "# Including:",
+    "# Excluding:",
+    "# ContextLines:",
+];
+const FLAGS = ["RegExp", "CaseSensitive", "IgnoreExcludeSettings", "WordMatch"];
 let cachedLastParse;
 let documentChangeListener;
 function activate(context) {
-    const contextLineDecorations = vscode.window.createTextEditorDecorationType({ opacity: '0.7' });
-    const matchLineDecorations = vscode.window.createTextEditorDecorationType({ fontWeight: 'bold' });
+    const contextLineDecorations = vscode.window.createTextEditorDecorationType({
+        opacity: "0.7",
+    });
+    const matchLineDecorations = vscode.window.createTextEditorDecorationType({
+        fontWeight: "bold",
+    });
     const decorate = (editor) => {
         const parsed = parseSearchResults(editor.document).filter(isResultLine);
-        const contextRanges = parsed.filter(line => line.isContext).map(line => line.prefixRange);
-        const matchRanges = parsed.filter(line => !line.isContext).map(line => line.prefixRange);
+        const contextRanges = parsed
+            .filter((line) => line.isContext)
+            .map((line) => line.prefixRange);
+        const matchRanges = parsed
+            .filter((line) => !line.isContext)
+            .map((line) => line.prefixRange);
         editor.setDecorations(contextLineDecorations, contextRanges);
         editor.setDecorations(matchLineDecorations, matchRanges);
     };
-    if (vscode.window.activeTextEditor && vscode.window.activeTextEditor.document.languageId === 'search-result') {
+    if (vscode.window.activeTextEditor &&
+        vscode.window.activeTextEditor.document.languageId === "search-result") {
         decorate(vscode.window.activeTextEditor);
     }
     context.subscriptions.push(vscode.languages.registerDocumentSymbolProvider(SEARCH_RESULT_SELECTOR, {
         provideDocumentSymbols(document, token) {
             const results = parseSearchResults(document, token)
                 .filter(isFileLine)
-                .map(line => new vscode.DocumentSymbol(line.path, '', vscode.SymbolKind.File, line.allLocations.map(({ originSelectionRange }) => originSelectionRange).reduce((p, c) => p.union(c), line.location.originSelectionRange), line.location.originSelectionRange));
+                .map((line) => new vscode.DocumentSymbol(line.path, "", vscode.SymbolKind.File, line.allLocations
+                .map(({ originSelectionRange }) => originSelectionRange)
+                .reduce((p, c) => p.union(c), line.location.originSelectionRange), line.location.originSelectionRange));
             return results;
-        }
+        },
     }), vscode.languages.registerCompletionItemProvider(SEARCH_RESULT_SELECTOR, {
         provideCompletionItems(document, position) {
             const line = document.lineAt(position.line);
             if (position.line > 3) {
                 return [];
             }
-            if (position.character === 0 || (position.character === 1 && line.text === '#')) {
+            if (position.character === 0 ||
+                (position.character === 1 && line.text === "#")) {
                 const header = Array.from({ length: DIRECTIVES.length }).map((_, i) => document.lineAt(i).text);
-                return DIRECTIVES
-                    .filter(suggestion => header.every(line => line.indexOf(suggestion) === -1))
-                    .map(flag => ({ label: flag, insertText: (flag.slice(position.character)) + ' ' }));
+                return DIRECTIVES.filter((suggestion) => header.every((line) => line.indexOf(suggestion) === -1)).map((flag) => ({
+                    label: flag,
+                    insertText: flag.slice(position.character) + " ",
+                }));
             }
-            if (line.text.indexOf('# Flags:') === -1) {
+            if (line.text.indexOf("# Flags:") === -1) {
                 return [];
             }
-            return FLAGS
-                .filter(flag => line.text.indexOf(flag) === -1)
-                .map(flag => ({ label: flag, insertText: flag + ' ' }));
-        }
-    }, '#'), vscode.languages.registerDefinitionProvider(SEARCH_RESULT_SELECTOR, {
+            return FLAGS.filter((flag) => line.text.indexOf(flag) === -1).map((flag) => ({ label: flag, insertText: flag + " " }));
+        },
+    }, "#"), vscode.languages.registerDefinitionProvider(SEARCH_RESULT_SELECTOR, {
         provideDefinition(document, position, token) {
             const lineResult = parseSearchResults(document, token)[position.line];
             if (!lineResult) {
                 return [];
             }
-            if (lineResult.type === 'file') {
+            if (lineResult.type === "file") {
                 return lineResult.allLocations;
             }
-            const translateRangeSidewaysBy = (r, n) => r.with({ start: new vscode.Position(r.start.line, Math.max(0, n - r.start.character)), end: new vscode.Position(r.end.line, Math.max(0, n - r.end.character)) });
-            return [Object.assign(Object.assign({}, lineResult.location), { targetSelectionRange: translateRangeSidewaysBy(lineResult.location.targetSelectionRange, position.character - 1) })];
-        }
+            const translateRangeSidewaysBy = (r, n) => r.with({
+                start: new vscode.Position(r.start.line, Math.max(0, n - r.start.character)),
+                end: new vscode.Position(r.end.line, Math.max(0, n - r.end.character)),
+            });
+            return [
+                Object.assign(Object.assign({}, lineResult.location), { targetSelectionRange: translateRangeSidewaysBy(lineResult.location.targetSelectionRange, position.character - 1) }),
+            ];
+        },
     }), vscode.languages.registerDocumentLinkProvider(SEARCH_RESULT_SELECTOR, {
         provideDocumentLinks(document, token) {
             return __awaiter(this, void 0, void 0, function* () {
                 return parseSearchResults(document, token)
-                    .filter(({ type }) => type === 'file')
-                    .map(({ location }) => ({ range: location.originSelectionRange, target: location.targetUri }));
+                    .filter(({ type }) => type === "file")
+                    .map(({ location }) => ({
+                    range: location.originSelectionRange,
+                    target: location.targetUri,
+                }));
             });
-        }
-    }), vscode.window.onDidChangeActiveTextEditor(editor => {
-        if ((editor === null || editor === void 0 ? void 0 : editor.document.languageId) === 'search-result') {
+        },
+    }), vscode.window.onDidChangeActiveTextEditor((editor) => {
+        if ((editor === null || editor === void 0 ? void 0 : editor.document.languageId) === "search-result") {
             // Clear the parse whenever we open a new editor.
             // Conservative because things like the URI might remain constant even if the contents change, and re-parsing even large files is relatively fast.
             cachedLastParse = undefined;
             documentChangeListener === null || documentChangeListener === void 0 ? void 0 : documentChangeListener.dispose();
-            documentChangeListener = vscode.workspace.onDidChangeTextDocument(doc => {
+            documentChangeListener = vscode.workspace.onDidChangeTextDocument((doc) => {
                 if (doc.document.uri === editor.document.uri) {
                     decorate(editor);
                 }
             });
             decorate(editor);
         }
-    }), { dispose() { cachedLastParse = undefined; documentChangeListener === null || documentChangeListener === void 0 ? void 0 : documentChangeListener.dispose(); } });
+    }), {
+        dispose() {
+            cachedLastParse = undefined;
+            documentChangeListener === null || documentChangeListener === void 0 ? void 0 : documentChangeListener.dispose();
+        },
+    });
 }
 exports.activate = activate;
 function relativePathToUri(path, resultsUri) {
     if (pathUtils.isAbsolute(path)) {
         return vscode.Uri.file(path);
     }
-    if (path.indexOf('~/') === 0) {
+    if (path.indexOf("~/") === 0) {
         return vscode.Uri.file(pathUtils.join(process.env.HOME, path.slice(2)));
     }
     if (vscode.workspace.workspaceFolders) {
         const multiRootFormattedPath = /^(.*) • (.*)$/.exec(path);
         if (multiRootFormattedPath) {
             const [, workspaceName, workspacePath] = multiRootFormattedPath;
-            const folder = vscode.workspace.workspaceFolders.filter(wf => wf.name === workspaceName)[0];
+            const folder = vscode.workspace.workspaceFolders.filter((wf) => wf.name === workspaceName)[0];
             if (folder) {
                 return vscode.Uri.file(pathUtils.join(folder.uri.fsPath, workspacePath));
             }
@@ -117,10 +147,10 @@ function relativePathToUri(path, resultsUri) {
         else if (vscode.workspace.workspaceFolders.length === 1) {
             return vscode.Uri.file(pathUtils.join(vscode.workspace.workspaceFolders[0].uri.fsPath, path));
         }
-        else if (resultsUri.scheme !== 'untitled') {
+        else if (resultsUri.scheme !== "untitled") {
             // We're in a multi-root workspace, but the path is not multi-root formatted
             // Possibly a saved search from a single root session. Try checking if the search result document's URI is in a current workspace folder.
-            const prefixMatch = vscode.workspace.workspaceFolders.filter(wf => resultsUri.toString().startsWith(wf.uri.toString()))[0];
+            const prefixMatch = vscode.workspace.workspaceFolders.filter((wf) => resultsUri.toString().startsWith(wf.uri.toString()))[0];
             if (prefixMatch) {
                 return vscode.Uri.file(pathUtils.join(prefixMatch.uri.fsPath, path));
             }
@@ -129,10 +159,12 @@ function relativePathToUri(path, resultsUri) {
     console.error(`Unable to resolve path ${path}`);
     return undefined;
 }
-const isFileLine = (line) => line.type === 'file';
-const isResultLine = (line) => line.type === 'result';
+const isFileLine = (line) => line.type === "file";
+const isResultLine = (line) => line.type === "result";
 function parseSearchResults(document, token) {
-    if (cachedLastParse && cachedLastParse.uri === document.uri && cachedLastParse.version === document.version) {
+    if (cachedLastParse &&
+        cachedLastParse.uri === document.uri &&
+        cachedLastParse.version === document.version) {
         return cachedLastParse.parse;
     }
     const lines = document.getText().split(/\r?\n/);
@@ -158,16 +190,24 @@ function parseSearchResults(document, token) {
                 targetUri: currentTarget,
                 originSelectionRange: new vscode.Range(i, 0, i, line.length),
             };
-            links[i] = { type: 'file', location, allLocations: currentTargetLocations, path };
+            links[i] = {
+                type: "file",
+                location,
+                allLocations: currentTargetLocations,
+                path,
+            };
         }
         if (!currentTarget) {
             continue;
         }
         const resultLine = RESULT_LINE_REGEX.exec(line);
         if (resultLine) {
-            const [, indentation, _lineNumber, seperator, resultIndentation] = resultLine;
+            const [, indentation, _lineNumber, seperator, resultIndentation,] = resultLine;
             const lineNumber = +_lineNumber - 1;
-            const resultStart = (indentation + _lineNumber + seperator + resultIndentation).length;
+            const resultStart = (indentation +
+                _lineNumber +
+                seperator +
+                resultIndentation).length;
             const metadataOffset = (indentation + _lineNumber + seperator).length;
             const location = {
                 targetRange: new vscode.Range(Math.max(lineNumber - 3, 0), 0, lineNumber + 3, line.length),
@@ -176,13 +216,18 @@ function parseSearchResults(document, token) {
                 originSelectionRange: new vscode.Range(i, resultStart, i, line.length),
             };
             currentTargetLocations === null || currentTargetLocations === void 0 ? void 0 : currentTargetLocations.push(location);
-            links[i] = { type: 'result', location, isContext: seperator === ' ', prefixRange: new vscode.Range(i, 0, i, metadataOffset) };
+            links[i] = {
+                type: "result",
+                location,
+                isContext: seperator === " ",
+                prefixRange: new vscode.Range(i, 0, i, metadataOffset),
+            };
         }
     }
     cachedLastParse = {
         version: document.version,
         parse: links,
-        uri: document.uri
+        uri: document.uri,
     };
     return links;
 }
--- out/test/suite/uitest.js
+++ out/test/suite/uitest.js
@@ -10,77 +10,83 @@ var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, ge
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 const vscode_extension_tester_1 = require("vscode-extension-tester");
-const assert = require('chai').assert;
-describe('Cloud 9 UI Tests', () => {
+const assert = require("chai").assert;
+describe("Cloud 9 UI Tests", () => {
     let driver;
     before(() => {
         driver = vscode_extension_tester_1.VSBrowser.instance.driver;
     });
-    it('End to End Test', function () {
+    it("End to End Test", function () {
         return __awaiter(this, void 0, void 0, function* () {
             this.timeout(100000);
             this.retries();
             const activityBar = new vscode_extension_tester_1.ActivityBar();
-            const control = yield activityBar.getViewControl('Cloud9');
+            const control = yield activityBar.getViewControl("Cloud9");
             yield control.openView();
             pauseForSeconds(1);
             //Click sign in on tree view
             const sidebar = new vscode_extension_tester_1.SideBarView();
-            const menuInfo = yield sidebar.getContent().getSection('Menu');
-            yield (yield menuInfo.findItem('Sign in / Create Account')).click();
+            const menuInfo = (yield sidebar
+                .getContent()
+                .getSection("Menu"));
+            yield (yield menuInfo.findItem("Sign in / Create Account")).click();
             //Click sign in on notfication
-            const signInNotif = (yield new vscode_extension_tester_1.Workbench().getNotifications());
+            const signInNotif = yield new vscode_extension_tester_1.Workbench().getNotifications();
             //Get info from nofication
             const message = yield signInNotif[0].getMessage();
-            assert.equal(message, 'Please sign in or create a new account!');
+            assert.equal(message, "Please sign in or create a new account!");
             //Click sign in and set email to be test@test.com
             yield signInNotif[0].takeAction((yield signInNotif[0].getActions())[0].getTitle());
             const input = yield vscode_extension_tester_1.InputBox.create();
-            yield input.setText('tester@test.com');
+            yield input.setText("tester@test.com");
             yield input.confirm(); // press enter
-            yield input.setText('password');
+            yield input.setText("password");
             yield input.confirm(); // press enter
-            assert.equal((yield (yield new vscode_extension_tester_1.Workbench().getNotifications())[0].getMessage()) == '', false);
+            assert.equal((yield (yield new vscode_extension_tester_1.Workbench().getNotifications())[0].getMessage()) == "", false);
             //View personal stats
-            console.log('1');
-            yield (yield menuInfo.findItem('📊 View personal stats')).click();
-            console.log('2');
+            console.log("1");
+            yield (yield menuInfo.findItem("📊 View personal stats")).click();
+            console.log("2");
             const personalStats = new vscode_extension_tester_1.TextEditor();
-            console.log('3');
+            console.log("3");
             const personalStatsTitle = yield personalStats.getTitle();
-            console.log('4');
+            console.log("4");
             assert(personalStatsTitle, "personal_statistics.txt");
             //View global leaderboard
-            console.log('5');
+            console.log("5");
             pauseForSeconds(1);
-            yield (yield menuInfo.findItem('🌐 Leaderboard')).click();
-            console.log('6');
-            const globalStats = yield new vscode_extension_tester_1.EditorView().openEditor('leaderboard.txt');
-            console.log('7');
+            yield (yield menuInfo.findItem("🌐 Leaderboard")).click();
+            console.log("6");
+            const globalStats = yield new vscode_extension_tester_1.EditorView().openEditor("leaderboard.txt");
+            console.log("7");
             assert.equal((yield globalStats.getTitle()) == "", false); // check actual text
-            console.log('8');
+            console.log("8");
             const editor = new vscode_extension_tester_1.TextEditor();
-            yield editor.typeText(1, 3, ' absolutely');
+            yield editor.typeText(1, 3, " absolutely");
             //console.log(await globalStats.getText()); */
             pauseForSeconds(2);
             //Load up team
-            console.log('9');
-            const teamInfo = yield sidebar.getContent().getSection('Team Info');
-            console.log('10');
+            console.log("9");
+            const teamInfo = (yield sidebar
+                .getContent()
+                .getSection("Team Info"));
+            console.log("10");
             //Check that testTeam is the team name
-            const getTeamInfo = yield (yield teamInfo.findItem('Get Team Info'));
+            const getTeamInfo = yield yield teamInfo.findItem("Get Team Info");
             yield getTeamInfo.click();
-            console.log('11');
+            console.log("11");
             const childItem = yield getTeamInfo.findChildItem("TeamName");
-            assert.equal((yield (yield (yield teamInfo.findItem('Get Team Info')).findChildItem("TeamName"))) == undefined, false);
+            assert.equal((yield yield (yield teamInfo.findItem("Get Team Info")).findChildItem("TeamName")) == undefined, false);
             console.log(12);
-            const dailyMetric = yield sidebar.getContent().getSection('Daily Metric');
+            const dailyMetric = (yield sidebar
+                .getContent()
+                .getSection("Daily Metric"));
             console.log(13);
             // Check that daily metric
-            assert.equal(yield (yield (yield dailyMetric.findItem('Keystrokes')).hasChildren()), true);
-            assert.equal(yield (yield (yield dailyMetric.findItem('Lines Changed')).hasChildren()), true);
-            assert.equal(yield (yield (yield dailyMetric.findItem('Time Interval')).hasChildren()), true);
-            assert.equal(yield (yield (yield dailyMetric.findItem('Total Points')).hasChildren()), true);
+            assert.equal(yield yield (yield dailyMetric.findItem("Keystrokes")).hasChildren(), true);
+            assert.equal(yield yield (yield dailyMetric.findItem("Lines Changed")).hasChildren(), true);
+            assert.equal(yield yield (yield dailyMetric.findItem("Time Interval")).hasChildren(), true);
+            assert.equal(yield yield (yield dailyMetric.findItem("Total Points")).hasChildren(), true);
         });
     });
 });
@@ -88,6 +94,6 @@ describe('Cloud 9 UI Tests', () => {
 function pauseForSeconds(seconds) {
     //Wait before clicking team
     var currentTime = new Date().getTime();
-    while (currentTime + (seconds * 1000) >= new Date().getTime()) { }
+    while (currentTime + seconds * 1000 >= new Date().getTime()) { }
 }
 //# sourceMappingURL=uitest.js.map
